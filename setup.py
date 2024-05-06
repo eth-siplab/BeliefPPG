d064d7394db5b1591a607f4a8c5b52cb0676f142
Paul Streli <paul@streli.eu>
2024-05-06 20:40:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README_PyPI.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='beliefppg',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     package_data={'beliefppg': ['inference/inference_model.keras', 'inference/inference_model_notimebackbone.keras']},
     install_requires=[
